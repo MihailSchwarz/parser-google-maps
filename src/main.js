4ad0a8a23dd220d7ddf420dacca587b88252965d
MihailSchwarz <mihawin@ya.ru>
2025-05-13 23:54:00 +0100

    bugfix

--- src/main.js
+++ src/main.js
@@ -32,7 +32,7 @@ async function runGoogleMaps(searchQuery) {
   try {
     // Запускаем браузер в headless режиме
     context = await chromium.launchPersistentContext(userDataDir, {
-      headless: false,
+      headless: true,
       viewport: { width: 1200, height: 900 },
       userAgent:
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
